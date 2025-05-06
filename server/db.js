16a0164c025f2f4354c96f0fbdff46cf919619ae
mirzaatifbaig <mirzaatifb91@gmail.com>
2025-05-06 23:55:54 +0500

    Finalized Project

--- server/db.js
+++ server/db.js
@@ -1,13 +1,15 @@
 // db.js
-const Database = require('better-sqlite3');
-const db = new Database('users.db');
+const Database = require("better-sqlite3");
+const db = new Database("users.db");
 
-db.prepare(`
+db.prepare(
+  `
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 )
-`).run();
+`,
+).run();
 
 module.exports = db;
